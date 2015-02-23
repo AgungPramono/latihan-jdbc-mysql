21759489c49acea7ba53d08d8809ec58ad538523
agung pramono <agungpermadi13@gmail.com>
2015-02-24 00:25:39 +0700

    Entity untuk menampung data

---
+++ src/main/java/com/agung/jdbc/Person.java
@@ -0,0 +1,50 @@
+/*
+ * Silahkan digunakan dengan bebas / dimodifikasi
+ * Dengan tetap mencantumkan nama @author dan Referensi / Source
+ * Terima Kasih atas Kerjasamanya.
+ */
+package com.agung.jdbc;
+
+/**
+ *
+ * @author Personal
+ */
+public class Person {
+    private Integer id = null;
+    private String nama = null;
+    private String alamat = null;
+    private String phone = null;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
